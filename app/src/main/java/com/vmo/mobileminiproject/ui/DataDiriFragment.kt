3f356f6e94e6ae1e7b56cc5c63d9c32643b1363d
hoangkien07051 <hoangkien07051@gmail.com>
2021-12-15 11:52:05 +0700

    view fragment diri

--- app/src/main/java/com/vmo/mobileminiproject/ui/DataDiriFragment.kt
+++ app/src/main/java/com/vmo/mobileminiproject/ui/DataDiriFragment.kt
@@ -1,7 +1,13 @@
 package com.vmo.mobileminiproject.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.vmo.mobileminiproject.R
 
 class DataDiriFragment : Fragment(R.layout.fragment_diri) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
